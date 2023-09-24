03f0d857a48b7be63cf38d42f5c3f2969c5cc5f5
Cnotd <2357676602@qq.com>
2023-09-24 10:59:45 +0800

    pro——new——1

---
+++ A_B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define IOS std :: ios::sync_with_stdio(0); std :: cin.tie(0); std ::cout.tie(0);
+#define PI 3.1415926
+using i64 = long long;
+
+void solve() {
+    i64 a , b ;
+     std :: cin >> a >> b ; 
+     std :: cout << a + b ;
+      std :: cout << std :: endl;
+      return ;
+}
+
+int main() {
+    IOS
+
+    int _t =  1;
+    //std :: cin >> _t;
+
+    while (_t--)
+        solve();
+}
